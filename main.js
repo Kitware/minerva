54f2f2cfb46786634788b7993e1667f450a6291a
Jonathan Beezley <jonathan.beezley@kitware.com>
2014-10-28 08:58:56 -0400

    Split apps into seperate files and attached events

--- main.js
+++ main.js
@@ -1,127 +1,46 @@
-// Globals
-var visdata = [];
 
 function main() {
-  var mapOptions =
-    {
-      node: '#map',
-      zoom : 4,
-      center : {x: -12, y: 8}
-    }, myMap = null,
-       layer = null,
-       tweetLayer = null,
-       tweetFeature = null;
 
-  /// Resize the canvas to fill browser window dynamically
-  window.addEventListener('resize', resizeCanvas, false);
+  var mapObj = createMap(),
+      currentState = null;
 
-  function updateAndDraw(width, height) {
-    if (!myMap) {
-      myMap = geo.map(mapOptions);
-      layer = myMap.createLayer('osm'),
-      tweetLayer = myMap.createLayer('feature');
-    }
-    myMap.resize(0, 0, width, height);
-    myMap.draw();
-  }
-
-  function resizeCanvas() {
-    updateAndDraw($('#map').width(), $('#map').height());
-  }
-
-  resizeCanvas();
-
-  var countries;
-
-  // start getting the country border data
-  d3.json('/countries.topo.json', function (error, data) {
-    if (error) {
-      console.log(error);
-      countries = null;
-      return;
-    }
-
-    countries = topojson.feature(data, data.objects.countries);
-  });
-
-  // save the current map state
-  var mapState = null,
-      featureLayer = myMap.createLayer('feature', {'renderer': 'd3Renderer'}),
-      svg = featureLayer.canvas(), // this is a d3 object wrapping an svg group element
-      renderer = featureLayer.renderer();
-
-  // Remove all features
-  function resetMap() {
-    svg.selectAll('*').remove();
-  }
-
-  function createChoroplethForTarget(target) {
-    if (countries === undefined) {
-      window.setTimeout(createChoropleth, 100);
-      return;
-    } else if (countries === null) {
-      console.log('Could not load country data');
-      return;
-    }
-
-    // find the country data for `target`
-    var feature;
-    countries.features.forEach(function (f) {
-      if (f.properties.name === target) {
-        feature = f;
+  // map state object, defines how to create/destroy features for each vis type
+  var states = {
+    twitter: {
+      layer: mapObj.gl,
+      visObj: window.app.twitter
+    },
+    choropleth: {
+      layer: mapObj.d3,
+      visObj: window.app.choropleth
+    },
+    unknown: { // Used for unimplemented components
+      layer: null,
+      visObj: {
+        create: function () {},
+        destroy: function () {}
       }
-    });
+    }
+  };
 
-    // draw the border
-    var line = d3.geo.path().projection(function (c) {
-      var d = renderer.worldToDisplay({
-        x: c[0],
-        y: c[1]
-      });
-      return [d.x, d.y];
-    });
-
-    svg.append('path')
-      .datum(feature)
-        .attr('d', line)
-        .attr('class', 'border')
-        .style('fill', 'yellow')
-        .style('fill-opacity', 0.25);
-
-    featureLayer.geoOn(geo.event.d3Rescale, function (arg) {
-      // TODO need to unbind on exit
-      svg.selectAll('.border')
-        .style('stroke-width', 1/arg.scale);
-    });
-  }
-
-  function createChoropleth() {
-      resetMap();
-      var targets = ['Guinea', 'Liberia', 'Sierra Leone', 'Nigeria', 'Senegal'];
-      targets.forEach(createChoroplethForTarget);
-  }
-
-  //$('#choropleth').click(createChoropleth);
-  createChoropleth();
-
-  // Test streams
-  var key = null;
-  tangelo.stream.start("example", function(d) { key = d; console.log(key);
-    tangelo.stream.run(key, function(data) {
-      data = eval(data);
-      if (data && data.length !== 0) {
-        visdata.push(data[0]);
-        if (tweetFeature) {
-          tweetLayer.deleteFeature(tweetFeature);
-        }
-        tweetFeature = tweetLayer.createFeature("point")
-          .data(visdata)
-          .position(function (d) { return { x: d.coordinates[1],
-                                            y: d.coordinates[0],
-                                            z: 0
-                                          }})
-        myMap.draw();
+  // main update function
+  function updateState(id) {
+    id = typeof id === 'string' ? id : $(this).attr('id');
+    if (!states.hasOwnProperty(id)) {
+      id = 'unknown';
+    }
+    if (id !== currentState) {
+      if (currentState) {
+        states[currentState].visObj.destroy();
       }
-    });
-  });
+      currentState = id;
+      states[currentState].visObj.create(mapObj.map, states[currentState].layer);
+    }
+  }
+
+  // Initialize
+  updateState($('.app-map-buttons button.active').attr('id'));
+
+  // attach to buttons
+  $('.app-map-buttons button').click(updateState);
 }
