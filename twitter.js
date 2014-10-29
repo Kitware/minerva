bf7f19e929038eab050fff7b314ff91d909e79f7
Aashish Chaudhary <aashish.chaudhary@kitware.com>
2014-10-29 02:19:11 -0400

    Changed name to minerva

--- twitter.js
+++ twitter.js
@@ -4,7 +4,7 @@
       key = null,
       map, layer;
 
-  tangelo.stream.start("example", function(d) { key = d; console.log(key);
+  tangelo.stream.start("minerva", function(d) { key = d; console.log(key);
     tangelo.stream.run(key, function(data) {
       data = JSON.parse(data);
       if (data && data.length !== 0) {
