b8ca1c101c1c6bfbcf17bf6b7d9f025edb887f72
Michael Grauer <michael.grauer@kitware.com>
2015-05-29 19:01:32 +0000

    Save zoom level in session baselayer

--- web_external/js/views/body/MapPanel.js
+++ web_external/js/views/body/MapPanel.js
@@ -1,6 +1,21 @@
 minerva.views.MapPanel = minerva.View.extend({
 
+    events: {
+        'click .m-save-current-baselayer': function () {
+            this.session.sessionJsonContents.center = this.map.center();
+            this.session.sessionJsonContents.zoom = this.map.zoom();
+            this.session.saveSession();
+        }
+    },
+
     addDataset: function (dataset) {
+        // TODO HACK
+        // deleting and re-adding ui layer to keep it on top
+        //this.map.deleteLayer(this.uiLayer);
+        // this causes a problem when there are at least two feature layers,
+        // so for now it is commented out
+        // this means we keep re-adding the ui layer each time a dataset is
+        // added as a feature layer, which is even more of a HACK
         var datasetId = dataset.id;
         if (!_.contains(this.datasets, datasetId)) {
             var layer,
@@ -19,6 +34,8 @@ minerva.views.MapPanel = minerva.View.extend({
                 complete: _.bind(function () {
                     layer.clear();
                     reader.read(data, _.bind(function () {
+                        this.uiLayer = this.map.createLayer('ui');
+                        this.uiLayer.createWidget('slider');
                         this.map.draw();
                     }, this));
                 }, this)
@@ -41,7 +58,7 @@ minerva.views.MapPanel = minerva.View.extend({
         girder.events.on('m:layerDatasetRemoved', this.removeDataset, this);
         this.session = settings.session;
         this.listenTo(this.session, 'm:mapUpdated', function () {
-            // TODO base layer changes, for now only dealing with center
+            // TODO for now only dealing with center
             if (this.map) {
                 // TODO could better separate geojs needs from session storage
                 this.map.center(this.session.sessionJsonContents.center);
@@ -54,18 +71,27 @@ minerva.views.MapPanel = minerva.View.extend({
         if (!this.map) {
             this.map = geo.map({
                 node: '.mapPanelMap',
-                center: this.session.sessionJsonContents.center
+                center: this.session.sessionJsonContents.center,
+                zoom: this.session.sessionJsonContents.zoom
             });
             this.map.createLayer(this.session.sessionJsonContents.basemap);
-            this.map.createLayer('ui')
-                .createWidget('slider');
-            this.map.draw();
+            this.uiLayer = this.map.createLayer('ui');
+            this.uiLayer.createWidget('slider');
+            window.map = this.map;
         }
+        this.map.draw();
     },
 
     render: function () {
         this.$el.html(minerva.templates.mapPanel());
         this.renderMap();
+        var tooltipProperties = {
+            placement: 'left',
+            delay: 400,
+            container: this.$el,
+            trigger: 'hover'
+        };
+        this.$('.m-save-current-baselayer').tooltip(tooltipProperties);
         return this;
     }
 });
