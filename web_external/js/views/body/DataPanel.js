40e87c6f005a446b89a2882f108fabc24f004475
Chris Harris <chris.harris@kitware.com>
2015-06-30 09:26:41 -0400

    Add data set types

--- web_external/js/views/body/DataPanel.js
+++ web_external/js/views/body/DataPanel.js
@@ -1,6 +1,6 @@
 minerva.views.DataPanel = minerva.View.extend({
     events: {
-        'click .m-add-dataset-button': 'uploadDialogEvent',
+        'click .m-add-dataset-button': 'addDataSetDialogEvent',
         'click .add-dataset-to-session': 'addDatasetToSessionEvent',
         'click .delete-dataset': 'deleteDatasetEvent',
         'click .csv-mapping': 'mapTableDataset',
@@ -110,111 +110,12 @@ minerva.views.DataPanel = minerva.View.extend({
         return this;
     },
 
-    // TODO should this uploader be broken out?
-    uploadDialogEvent: function () {
+    addDataSetDialogEvent: function () {
         var container = $('#g-dialog-container');
 
-        this.uploadWidget = new girder.views.UploadWidget({
+        this.addDataSetWidget = new minerva.views.AddDataSetWidget({
             el: container,
-            noParent: true,
-            title: 'Upload a new dataset',
-            overrideStart: true,
             parentView: this
-        }).on('g:uploadFinished', function () {
-            girder.dialogs.handleClose('upload');
-            this.upload = false;
-        }, this).render();
-
-        this.$('input.m-shapefile-item-name').focus();
-        this.listenTo(this.uploadWidget, 'g:filesChanged', this.filesSelected);
-        this.listenTo(this.uploadWidget, 'g:uploadStarted', this.uploadStarted);
-        this.listenTo(this.uploadWidget, 'g:uploadFinished', this.uploadFinished);
-
+        }).render();
     },
-
-    // any shapefile upload must contain files with these extensions
-    _shapefileRequired: ['.shp', '.shx', '.dbf'],
-
-    /**
-     * Called when the user selects or drops files to be uploaded.
-     */
-    filesSelected: function (files) {
-        var zeroethFileName = null;
-        this.newItemName = null;
-        this.newItemExt = null;
-        if (this.validateShapefileExtensions) {
-            // get a list of file extensions in the selected files
-            var fileExts = _.filter(_.map(files, function (file) {
-                var dotPos = file.name.lastIndexOf('.');
-                if (dotPos === -1) {
-                    return '';
-                } else {
-                    return file.name.substr(dotPos);
-                }
-            }), function (ext) {
-                return ext !== '';
-            });
-
-            // ensure that every one of the required extensions exists
-            // somewhere in the list of selected files
-            this.shapefileContentsOk =
-                _.every(this._shapefileRequired, function (ext) {
-                    return _.contains(fileExts, ext);
-                });
-
-            // get the name for the item from the .shp
-            var shapefile = _.find(files, function (file) {
-                var dotPos = file.name.lastIndexOf('.');
-                return (dotPos > -1 && file.name.substr(dotPos) === '.shp');
-            });
-            if (shapefile && shapefile.name.lastIndexOf('.') > -1) {
-                this.newItemName = shapefile.name.substr(0, shapefile.name.lastIndexOf('.'));
-                this.newItemExt = zeroethFileName.substr(zeroethFileName.lastIndexOf('.'), zeroethFileName.length);
-            }
-
-            this.uploadWidget.setUploadEnabled(this.shapefileContentsOk);
-            if (!this.shapefileContentsOk) {
-                $('.g-upload-error-message').text(
-                    'You must include one file of each type: [' + this._shapefileRequired.join(', ') + ']');
-                $('.g-overall-progress-message').empty();
-            } else {
-                $('.g-upload-error-message').empty();
-            }
-        } else {
-            // take the new item's name from the first file
-            if (files && files.length > 0) {
-                zeroethFileName = files[0].name;
-                this.newItemName = zeroethFileName.substr(0, zeroethFileName.lastIndexOf('.'));
-                this.newItemExt = zeroethFileName.substr(zeroethFileName.lastIndexOf('.'), zeroethFileName.length);
-            }
-        }
-    },
-
-    /**
-     * Create a new Item for the shapefile under
-     * the minerva folder, then upload all files into that Item.
-     */
-    uploadStarted: function () {
-        // need to create a new item in the dataset folder, then upload there
-        this.newDataset = new minerva.models.DatasetModel({
-            name: this.newItemName,
-            folderId: this.collection.datasetFolderId
-        }).on('g:saved', function () {
-            this.uploadWidget.parentType = 'item';
-            this.uploadWidget.parent = this.newDataset;
-            this.uploadWidget.uploadNextFile();
-        }, this).on('g:error', function (err) {
-            console.error(err);
-        }).save();
-    },
-
-    /**
-     * Post-process data after it has been loaded depending on the
-     * extension of the dataset.
-     */
-    uploadFinished: function () {
-        this.newDataset.on('m:datasetCreated', function (dataset) {
-            this.collection.add(dataset);
-        }, this).createDataset();
-    }
 });
