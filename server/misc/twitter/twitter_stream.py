ffbbaf18e412820b51f505a9fddb04b64ab635ee
Michael Grauer <michael.grauer@kitware.com>
2015-07-09 14:58:26 +0000

    Pass search term on cmd line and cache resolver

--- server/misc/twitter/twitter_stream.py
+++ server/misc/twitter/twitter_stream.py
@@ -1,5 +1,5 @@
 # Requires tweepy (pip install tweepy)
-from tweepy.streaming import StreamListener
+from tweepy import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
 
@@ -9,7 +9,6 @@ import time
 import json
 import atexit
 import pymongo
-import inspect
 
 from libs.carmen import get_resolver
 
@@ -19,21 +18,22 @@ minerva_twitter_config = json.load(open(
     os.path.join(os.path.dirname(__file__), "twitter.json")
 ))
 
+
 class TwitterStreamListener(StreamListener):
     """ A listener handles tweets are the received from the stream.
     This is a basic listener that just prints received tweets to stdout.
 
     """
-    def __init__(self):
+    def __init__(self, query):
         """Initialize twitter stream"""
         StreamListener.__init__(self)
-        self._mongo = pymongo.MongoClient().minerva.tweets
+        self._mongo = pymongo.MongoClient().minerva[query]
         self._filters = []
 
     def add_filter(self, filter):
         """A filter takes JSON  as input and outputs a JSON as well"""
         if filter not in self._filters:
-            self._filters.append(filter);
+            self._filters.append(filter)
 
     def on_data(self, data):
         """Receive tweet and save it to the database"""
@@ -53,9 +53,11 @@ class TwitterStreamListener(StreamListener):
                 "text": json_data['text'],
                 "timestamp_ms": json_data['timestamp_ms'],
                 "created_at": json_data['created_at'],
-                "retweeted" : json_data['retweeted'],
-                "retweet_cont" : retweet_cont
+                "retweeted": json_data['retweeted'],
+                "retweet_cont": retweet_cont
             }
+
+            print(rec)
 
             # Insert data in mongodb
             self._mongo.insert(rec)
@@ -64,7 +66,7 @@ class TwitterStreamListener(StreamListener):
 
     def on_error(self, status):
         """Handle any error encountered during streaming"""
-        print status
+        print(status)
 
     def on_timeout(self):
         sys.stderr.write("Timeout, sleeping for 60 seconds...\n")
@@ -73,32 +75,38 @@ class TwitterStreamListener(StreamListener):
 
 _exit = False
 
+
 def exitHandler():
     global _exit
     _exit = True
 
 atexit.register(exitHandler)
 
-def tweetGeocoder(tweet):
+
+def createTweetGeocoder():
     resolver = get_resolver()
     resolver.load_locations()
-    location = resolver.resolve_tweet(tweet)
-    if location is not None:
-        tweet["location"] = location[1].__dict__
-    return tweet
+
+    def tweetGeocoder(tweet):
+        location = resolver.resolve_tweet(tweet)
+        if location is not None:
+            tweet["location"] = location[1].__dict__
+        return tweet
+
+    return tweetGeocoder
 
 
-def stream():
-    listn = TwitterStreamListener()
-    listn.add_filter(tweetGeocoder)
+def stream(query):
+    listn = TwitterStreamListener(query)
+    listn.add_filter(createTweetGeocoder())
 
     auth = OAuthHandler(minerva_twitter_config["twitter"]["CONSUMER_KEY"],
                         minerva_twitter_config["twitter"]["CONSUMER_SECRET"])
     auth.set_access_token(minerva_twitter_config["twitter"]["ACCESS_KEY"],
                           minerva_twitter_config["twitter"]["ACCESS_SECRET"])
     stream = Stream(auth, listn)
-    stream.filter(track=['ebola'], async=False)
+    stream.filter(track=[query], async=False)
 
 if __name__ == '__main__':
-    stream()
+    stream(sys.argv[1])
 
