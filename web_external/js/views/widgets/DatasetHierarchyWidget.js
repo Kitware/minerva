691720a9178f85cd681f66a12febf67b21a92139
Christopher Kotfila <chris.kotfila@kitware.com>
2015-07-23 11:07:17 -0400

    Adding s3 hierarchy widget
    This is just the basic widget,  needs some config work to remove
    unnessisary widgets for this application

--- web_external/js/views/widgets/DatasetHierarchyWidget.js
+++ web_external/js/views/widgets/DatasetHierarchyWidget.js
@@ -3,23 +3,47 @@
 */
 minerva.views.DatasetHierarchyWidget = minerva.View.extend({
     initialize: function (settings) {
-        this.dataset = settings.dataset;
+        // this.folderAccess = settings.folderAccess || false;
+        this.folderAccess = false;
+
+        this.folder = new girder.models.FolderModel();
+        this.folder.set({
+            "_id": settings.folderID
+        })
+
+this.folder.on('g:fetched', function () {
+            this._createHierarchyWidget();
+            this.render();
+        }, this).on('g:error', function () {
+            this.folder = null;
+            this._createHierarchyWidget();
+            this.render();
+        }, this).fetch();
+
+    },
+    _createHierarchyWidget: function () {
+        this.hierarchyWidget = new girder.views.HierarchyWidget({
+            parentModel: this.folder,
+            folderAccess: this.folderAccess,
+            upload: false,
+            folderCreate: false,
+            folderEdit: false,
+            itemCreate: false,
+            parentView: this
+        });
     },
 
     render: function () {
         var modal = this.$el.html(minerva
-				  .templates
-				  .datasetHierarchyWidget({}))
-		.girderModal(this)
-		.on('ready.girder.modal',
-		    _.bind(function () {
-			this.$('#datasetHeirarchy')
-			    .text(JSON.stringify(
-				this.dataset.get('meta').minerva,
-				null, 4));
-		    }, this));
+                                  .templates
+                                  .datasetHierarchyWidget({}))
+                .girderModal(this)
+                .on('ready.girder.modal',
+                    _.bind(function () {
+                        this.hierarchyWidget.setElement("#datasetHeirarchy").render();
+                    }, this));
 
-	modal.trigger($.Event('ready.girder.modal', {relatedTarget: modal}));
+        modal.trigger($.Event('ready.girder.modal', {relatedTarget: modal}));
 
         return this;
     }
