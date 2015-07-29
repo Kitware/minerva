9a76a57f9add2661e771cfec78643d9cfb74fca3
Christopher Kotfila <chris.kotfila@kitware.com>
2015-07-29 09:59:45 -0400

    Reintroduce polymorphic dataset models
    S3 model creationg got clobbered in the move to
    minerva.collections.DatasetCollection extending the new
    minerva.collections.MinervaCollection.

--- web_external/js/collections/DatasetCollection.js
+++ web_external/js/collections/DatasetCollection.js
@@ -1,6 +1,14 @@
 minerva.collections.DatasetCollection = minerva.collections.MinervaCollection.extend({
 
-    model: minerva.models.DatasetModel,
+    //model: minerva.models.DatasetModel,
+    model: function (attrs, options) {
+        if (attrs.meta && ('minerva' in attrs.meta) &&
+            attrs.meta.minerva.original_type === 's3') {
+            return new minerva.models.S3DatasetModel(attrs, options);
+        } else {
+            return new minerva.models.DatasetModel(attrs, options);
+        }
+    },
     path: 'minerva_dataset',
     getInitData: function () {
         var initData = { userId: girder.currentUser.get('_id') };
