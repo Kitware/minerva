7eb3117ad7747ade4f1ad51012acfc9789914012
Christopher Kotfila <chris.kotfila@kitware.com>
2015-08-13 08:41:01 -0400

    Add debugging into contour.py
    Just give us some timing feedback in the romanesco/celery log

--- analyses/NEX/geo_js_contour/contour.py
+++ analyses/NEX/geo_js_contour/contour.py
@@ -3,17 +3,26 @@ import tempfile
 import shutil
 from netCDF4 import Dataset
 from bson import json_util
-
+from contextlib import contextmanager
 from girder_client import GirderClient
+import time
+
+def debug(s):
+    print s
+
+@contextmanager
+def timer(s):
+    t0 = time.time()
+    yield
+    print "%s (%.2f)" % (s, time.time() - t0)
 
 def convert(data_path, variable, timestep):
     data = Dataset(data_path)
     variable = data.variables[variable]
     shape = variable[timestep].shape
 
-    # For now sub select ( take about 10% of the grid )
-    lat_select_index = shape[0] / 10
-    lon_select_index = shape[1] / 10
+    lat_select_index = shape[0]
+    lon_select_index = shape[1]
 
     # Extract out the lat lon names
     dimensions = map(lambda d : d, data.dimensions)
@@ -35,6 +44,7 @@ def convert(data_path, variable, timestep):
 
     return contour_data
 
+debug("Starting contour task")
 client = GirderClient(host, port)
 client.token = token
 
@@ -58,26 +68,33 @@ output_file_name = input_file_name.replace('.nc', '.json')
 
 try:
     # Now download the dataset
+    
     (fd, filepath) = tempfile.mkstemp()
     os.close(fd)
-    client.downloadFile(fileId, filepath)
+
+    with timer("Downloaded file %s" % filepath):
+        client.downloadFile(fileId, filepath)
 
     # Create temp file and convert to GeoJs contour JSON format
-    output_dir = tempfile.mkdtemp()
-    output_filepath = os.path.join(output_dir, output_file_name)
-    with open(output_filepath, 'w') as fp:
-        fp.write(json_util.dumps(convert(filepath, variable, timestep)))
+    with timer("Converted file %s" % filepath):
+        output_dir = tempfile.mkdtemp()
+        output_filepath = os.path.join(output_dir, output_file_name)
+        with open(output_filepath, 'w') as fp:
+            fp.write(json_util.dumps(convert(filepath, variable, timestep)))
 
     # Create an item for this file
-    output_item = client.createItem(dataset_folder_id, output_file_name, output_file_name)
+    with timer("Created item for file"):
+        output_item = client.createItem(dataset_folder_id, output_file_name, output_file_name)
 
     # Now upload the result
-    client.uploadFileToItem(output_item['_id'], output_filepath)
+    with timer("Uploaded file %s to %s" % (output_filepath, output_item['_id'])):
+        client.uploadFileToItem(output_item['_id'], output_filepath)
 
     output_item_id = output_item['_id']
 
     # Finally promote item to dataset
-    client.post('minerva_dataset/%s/dataset' % output_item_id)
+    with timer("Promoted item %s to dataset" % output_item_id):
+        client.post('minerva_dataset/%s/dataset' % output_item_id)
 
 finally:
     if filepath and os.path.exists(filepath):
