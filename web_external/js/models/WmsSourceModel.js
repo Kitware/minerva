afaae666dbf8c563432fbac9a8439247bdbb8072
Michael Grauer <michael.grauer@kitware.com>
2015-09-03 18:17:40 +0000

    Add client SourceModel and WmsSourceModel

---
+++ web_external/js/models/WmsSourceModel.js
@@ -0,0 +1,25 @@
+minerva.models.WmsSourceModel = minerva.models.SourceModel.extend({
+
+    createSource: function (name, baseURL, projection) {
+        var params = {
+            'name': name,
+            'baseURL': baseURL,
+            'projection': projection
+        };
+        girder.restRequest({
+            path: '/minerva_source/wms_source',
+            type: 'POST',
+            data: params,
+            error: null // ignore default error behavior (validation may fail)
+        }).done(_.bind(function (resp) {
+            this.set(resp);
+            this.trigger('m:saved');
+        }, this)).error(_.bind(function (err) {
+            this.trigger('m:error', err);
+        }, this));
+
+        return this;
+    },
+
+
+});
