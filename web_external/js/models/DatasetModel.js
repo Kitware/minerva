3af9a2a3639f73ae341894cc9ea1f1cb53783bad
Michael Grauer <michael.grauer@kitware.com>
2015-09-04 15:21:15 +0000

    Deduplicate MinervaModel methods from DatasetModel

--- web_external/js/models/DatasetModel.js
+++ web_external/js/models/DatasetModel.js
@@ -6,16 +6,6 @@ minerva.models.DatasetModel = minerva.models.MinervaModel.extend({
         files: null
     },
 
-    // TODO put in a toJson method ignoring displayed and other state
-
-    initialize: function () {
-        // populate the dataset with minerva metadata
-        var minervaMetadata = this.getMinervaMetadata();
-        if (minervaMetadata) {
-            this.setMinervaMetadata(minervaMetadata);
-        }
-    },
-
     isRenderable: function () {
         // Really this function should be defined in each data model subclass,
         // OR - based on whether or not geoFileReader is defined (better because
@@ -103,57 +93,6 @@ minerva.models.DatasetModel = minerva.models.MinervaModel.extend({
         }).done(_.bind(function (resp) {
             this.setMinervaMetadata(resp);
             this.trigger('m:externalMongoLimitsGot', this);
-        }, this));
-    },
-
-    //
-    // this is the start of trying to build an interface around the minerva metadata
-    // TODO what assumptions/error handling to make for minerva metadata
-    //
-    //
-
-    getMinervaMetadata: function () {
-        // for now assume that keys exists and allow exceptions to happen if they don't
-        var meta = this.get('meta');
-        if (meta) {
-            var minervaMetadata = meta.minerva;
-            return minervaMetadata;
-        } else {
-            return false;
-        }
-    },
-
-    setMinervaMetadata: function (minervaMetadata) {
-        this.set('meta', _.extend(this.get('meta') || {}, {minerva: minervaMetadata}));
-        // TODO may want an extend minervaMetadata, this one replaces
-        if (minervaMetadata.geojson_file || minervaMetadata.geojson) {
-            this.geoJsonAvailable = true;
-        }
-        if (minervaMetadata.geojson && minervaMetadata.geojson.data) {
-            this.fileData = minervaMetadata.geojson.data;
-        }
-        return minervaMetadata;
-    },
-
-    saveMinervaMetadata: function (minervaMetadata) {
-        if (minervaMetadata) {
-            this.setMinervaMetadata(minervaMetadata);
-        }
-        girder.restRequest({
-            path: 'item/' + this.get('_id') + '/metadata',
-            type: 'PUT',
-            contentType: 'application/json',
-            data: JSON.stringify(this.get('meta'))
-        }).done(_.bind(function () {
-            this.trigger('m:minervaMetadataSaved', this);
-        }, this)).error(_.bind(function (err) {
-            console.error(err);
-            girder.events.trigger('g:alert', {
-                icon: 'cancel',
-                text: 'Could not update Minera metadata in dataset item.',
-                type: 'error',
-                timeout: 4000
-            });
         }, this));
     },
 
