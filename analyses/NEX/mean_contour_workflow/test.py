35499394b25c3697becf83438e8401294aa387bd
Christopher Kotfila <chris.kotfila@kitware.com>
2015-09-17 15:34:31 -0400

    Example of end to end workflow

--- analyses/NEX/mean_contour_workflow/test.py
+++ analyses/NEX/mean_contour_workflow/test.py
@@ -14,22 +14,40 @@ if __name__ == "__main__":
     c = GirderClient('localhost', 8081)
     c.authenticate("kotfic", "letmein")
 
+
+
     output = romanesco.run(
-        {
-            "inputs": [{"name": "input", "type": "string", "format": "text"}],
-            "outputs": [{"name": "out", "type": "string", "format": "json"}],
-            "script": "with open (input, 'rb') as fh:\n    out = fh.read()",
-            "mode": "python"
-        },
+        wf,
+
         inputs={
-            "input": {
-                'format': 'text',
-                'data': "/home/kotfic/kitware/projects/NEX/data/points.csv"},
-        },
-        outputs={
-            'out': {
+# Cached file
+#           "file_path": {
+#               'format': 'text',
+#               'data': "/home/kotfic/kitware/projects/NEX/data/test.nc"},
+
+            "file_path": {
                 'mode': 'girder',
-                'name': 'points.csv',
+                'host': 'localhost',
+                'scheme': 'http',
+                'port': 8081,
+                'api_root': '/api/v1',
+                'resource_type': 'file',
+                'id': "55df4fbaf4b1496693a3c464",
+                'type': 'string',
+                'format': 'text',
+                "name": "test.nc",
+                'token': c.token
+            },
+            "m.variable": {
+                "format": "text",
+                "data": "pr"},
+            "c.variable": {
+                "format": "text",
+                "data": "pr"}},
+        outputs={
+            'contour': {
+                'mode': 'girder',
+                'name': 'test.json',
                 'host': 'localhost',
                 'scheme': 'http',
                 'port': 8081,
@@ -39,49 +57,36 @@ if __name__ == "__main__":
                 'format': 'json',
                 'type': 'string',
                 'token': c.token
-            }
-        }
-
-    )
-
-
-#    output = romanesco.run(
-#        wf,
-#        inputs={
-#            "file_path": {
-#                'mode': 'girder',
-#                'host': 'localhost',
-#                'scheme': 'http',
-#                'port': 8081,
-#                'api_root': '/api/v1',
-#                'resource_type': 'file',
-#                'id': "55df4fbaf4b1496693a3c464",
-#                'type': 'string',
-#                'format': 'text',
-#                "name": "test.nc",
-#                'token': c.token
-#            },
-#            "m.variable": {
-#                "format": "text",
-#                "data": "pr"},
-#            "c.variable": {
-#                "format": "text",
-#                "data": "pr"}},
-#        outputs={
-#            'contour': {
-#                'mode': 'girder',
-#                'name': 'test.json',
-#                'host': 'localhost',
-#                'scheme': 'http',
-#                'port': 8081,
-#                'api_root': '/api/v1',
-#                'parent_type': 'folder',
-#                'parent_id': "55b13466f4b149110f800aa1",
-#                'format': 'json',
-#                'type': 'string',
-#                'token': c.token
-#            }})
-
-#    "file_path": {
-#        'format': 'text',
-#        'data': "/home/kotfic/kitware/projects/NEX/data/test.nc"},
+            }})
+####
+#  For testing upload and item=>dataset promotion
+#
+#     output = romanesco.run(
+#         {
+#             "inputs": [{"name": "input", "type": "string", "format": "text"}],
+#             "outputs": [{"name": "out", "type": "string", "format": "json"}],
+#             "script": "with open (input, 'rb') as fh:\n    out = fh.read()",
+#             "mode": "python"
+#         },
+#         inputs={
+#             "input": {
+#                 'format': 'text',
+#                 'data': "/home/kotfic/kitware/projects/NEX/data/points.csv"},
+#         },
+#         outputs={
+#             'out': {
+#                 'mode': 'girder',
+#                 'name': 'points.csv',
+#                 'host': 'localhost',
+#                 'scheme': 'http',
+#                 'port': 8081,
+#                 'api_root': '/api/v1',
+#                 'parent_type': 'folder',
+#                 'parent_id': "55b13466f4b149110f800aa1",
+#                 'format': 'json',
+#                 'type': 'string',
+#                 'token': c.token
+#             }
+#         }
+#
+#     )
