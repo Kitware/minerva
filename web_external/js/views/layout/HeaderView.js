7ceb9700f7a14587d90b973c4d6c91c9c0cb5765
katelynnobrien <kate@epidemico.com>
2016-02-29 20:06:02 +0000

    fix travis eslist complaint

--- web_external/js/views/layout/HeaderView.js
+++ web_external/js/views/layout/HeaderView.js
@@ -1,13 +1,10 @@
 minerva.views.LayoutHeaderView = minerva.View.extend({
     events: {
     },
-
     render: function () {
-
         this.$el.html(minerva.templates.layoutHeader({
             staticRoot: girder.staticRoot
         }));
-
         new minerva.views.LayoutHeaderUserView({
             el: this.$('.m-current-user-wrapper'),
             parentView: this
