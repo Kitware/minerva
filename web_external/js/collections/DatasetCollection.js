56ec8f407da24c3580a1a57f70a27c17e3a2813f
mgrauer <michael.grauer@kitware.com>
2016-04-01 15:20:39 +0000

    First integration with wfs dataset

--- web_external/js/collections/DatasetCollection.js
+++ web_external/js/collections/DatasetCollection.js
@@ -5,6 +5,8 @@ minerva.collections.DatasetCollection = minerva.collections.MinervaCollection.ex
         if (attrs.meta && ('minerva' in attrs.meta)) {
             if (attrs.meta.minerva.dataset_type === 'wms') {
                 return new minerva.models.WmsDatasetModel(attrs, options);
+            } else if (attrs.meta.minerva.dataset_type === 'wfs') {
+                return new minerva.models.WfsDatasetModel(attrs, options);
             }
         }
         return new minerva.models.DatasetModel(attrs, options);
