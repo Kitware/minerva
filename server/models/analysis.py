3e86c6614b294301e119b852a4b51f98bd8b6402
Christopher Kotfila <chris.kotfila@kitware.com>
2016-04-20 13:52:48 -0400

    Add Update,  ensure name is unique

--- server/models/analysis.py
+++ server/models/analysis.py
@@ -9,12 +9,13 @@ from girder.models.model_base import AccessControlledModel
 class Analysis(AccessControlledModel):
     def initialize(self):
         self.name = "minerva_analysis"
+        self.ensureIndex(('name', {'unique': True}))
 
     def validate(self, doc):
         # TODO ensure file exists
         # TODO ensure run() function is available
+        # TODO ensure name is unique
         return doc
-
 
     def list(self, user=None, filters=None, sort=None):
         if user is None:
@@ -22,24 +23,32 @@ class Analysis(AccessControlledModel):
 
         cursor = self.find(filters or {}, limit=0, sort=sort)
 
-        return list(self.filterResultsByPermission(cursor=cursor, user=user,
-                                                   level=AccessType.READ,
-                                                   limit=None, offset=None))
+        return list(self.filterResultsByPermission(
+            cursor=cursor, user=user,
+            level=AccessType.READ,
+            limit=None, offset=None))
 
     def remove(self, analysis):
         AccessControlledModel.remove(self, analysis)
 
-    def create(self, path, analysis_type, user=None):
+    def update(self, analysis):
+        analysis['updated'] = datetime.datetime.utcnow()
+        return self.save(analysis)
+
+    def create(self, path, analysis_type, user=None, name=None):
         if user is None:
             user = getCurrentUser()
 
         now = datetime.datetime.utcnow()
 
+        if name is None:
+            name = os.path.splitext(os.path.basename(path))[0]
+
         if '_id' not in user:
             raise Exception("user must have an '_id' property set")
 
         return self.save({
-            'name': os.path.basename(path),
+            'name': name,
             'path': path,
             'type': analysis_type,
             'userId': user['_id'],
