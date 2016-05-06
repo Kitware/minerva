8c518947b79d9ed36db40917a2cc2a70f1602ecd
Jonathan Beezley <jonathan.beezley@kitware.com>
2016-05-06 11:31:26 -0400

    Use one view object per style model

---
+++ web_external/js/views/widgets/GeoJSONStyleElement.js
@@ -0,0 +1,182 @@
+minerva.models.GeoJSONProperty = Backbone.Model.extend({
+    defaults: {
+        key: null,
+        values: {},
+        min: null,
+        max: null
+    }
+});
+
+/* GeoJSON property accumulation object:
+ *
+ *  {
+ *    <key>: {
+ *      // for numeric values:
+ *      min: <minimum>,
+ *      max: <maximum>,
+ *
+ *      // for other value types:
+ *      values: {
+ *        <value>: <count>
+ *      }
+ *    }
+ *  }
+ *
+ */
+
+minerva.models.GeoJSONStyle = Backbone.Model.extend({
+    defaults: {
+        // how is the style generated
+        scale: 'constant',   // 'constant' | 'continuous' | 'categorical'
+
+        // what type is the range
+        type: 'number',     // 'number' | 'color'
+
+        // for color values, what color color ramp is selected
+        ramp: 'Reds',
+
+        // which style attribute does it represent
+        name: null,         // 'fillColor' | 'fillOpacity' | etc...
+
+        // which property key is the scale applied to
+        key: null,
+
+        // the geojson summary object
+        summary: {},
+
+        // for continuous scales
+        domain: [],         // numeric extent of the domain
+        range: [],          // numeric or color extent of the range
+
+        // for categorical scales
+        values: {},         // value -> (number | color) mapping
+
+        // for constant scales and default value
+        value: null
+    },
+
+    /**
+     * Custom set method that ignore undefined values.
+     */
+    set: function (hash, options) {
+        var key, value;
+
+        // handle set(key, value) calling
+        if (_.isString(hash)) {
+            key = hash;
+            value = options;
+            options = arguments[2];
+            hash = {};
+            hash[key] = value;
+        }
+
+        // remove keys with undefined values
+        hash = _.pick(hash, function (value, key) {
+            return value !== undefined;
+        });
+
+        // call super method
+        return Backbone.Model.prototype.set.call(this, hash, options);
+    },
+
+    /**
+     * Return a d3 scale object that performs the style mapping indicated
+     * by this model's attributes.
+     */
+    scale: function () {
+        var scale, value, pairs;
+
+        switch (this.get('type')) {
+            case 'constant':
+                value = this.get('value');
+                scale = function () { return value; };
+                break;
+
+            case 'continuous':
+                scale = d3.scale.linear()
+                    .domain(this.get('domain'))
+                    .range(this.get('range'));
+                break;
+
+            case 'categorical':
+                pairs = _.pairs(this.get('values'));
+                scale = d3.scale.ordinal()
+                    .domain(pairs.map(_.first))
+                    .range(pairs.map(_.last));
+                break;
+
+            default:
+                throw new Error('Invalid style type');
+        }
+        return scale;
+    },
+
+    scales: ['constant', 'continuous', 'categorical'],
+    ramps: _.map(colorbrewer, _.bind(function (ramp, name) {
+            var n = "<ul class='m-color-ramp'>";
+            _.each(ramp[6], function (color, i) {
+                n += "<li style='background-color: " + color + "'/>";
+            });
+            n += '</ul>';
+            this[name] = {
+                value: ramp[6],
+                display: n
+            };
+            return this;
+    }, {}))[0]
+
+});
+
+minerva.collections.GeoJSONStyle = Backbone.Collection.extend({
+    model: minerva.models.GeoJSONStyle
+});
+
+minerva.views.GeoJSONStyleElement = minerva.View.extend({
+    events: {
+        'change input,select': 'read'
+    },
+    initialize: function () {
+        this.listenTo(this.model, 'change', this.render);
+        this.listenTo(this.model, 'destroy', this.remove);
+    },
+    render: function (evt) {
+
+        // when responding to an input element change event, don't re-render
+        if (evt && evt.norender) {
+            return;
+        }
+
+        this.$el.html(minerva.templates.geoJSONStyleElement({
+            style: this.model.attributes,
+            ramps: this.model.ramps,
+            scales: this.model.scales
+        }));
+
+        // initialize bootstrap select elements
+        this.$el.find('select.m-select-ramp').selectpicker({width: '100%'});
+        return this;
+    },
+
+    remove: function () {
+        this.$('select.m-select-ramp').selectpicker('destroy');
+        this.$el.empty();
+        return this;
+    },
+
+    /**
+     * Read the value of the DOM element and set the model.
+     */
+    read: function () {
+        var $el = this.$el;
+        var attrs = {
+            scale: $el.find('.m-scale-type').val(),
+            key: $el.find('.m-scale-by').val(),
+            value: $el.find('.m-constant-value').val(),
+            range: [$el.find('.m-scale-min').val(), $el.find('.m-scale-max').val()],
+            ramp: $el.find('select.m-select-ramp').val()
+        };
+
+        this.model.set(attrs, {norender: true});
+        return this;
+    }
+});
--- web_external/js/views/widgets/GeoJSONStyleWidget.js
+++ web_external/js/views/widgets/GeoJSONStyleWidget.js
@@ -1,127 +1,6 @@
-minerva.models.GeoJSONProperty = Backbone.Model.extend({
-    defaults: {
-        key: null,
-        values: {},
-        min: null,
-        max: null
-    }
-});
-
-/* GeoJSON property accumulation object:
- *
- *  {
- *    <key>: {
- *      // for numeric values:
- *      min: <minimum>,
- *      max: <maximum>,
- *
- *      // for other value types:
- *      values: {
- *        <value>: <count>
- *      }
- *    }
- *  }
- *
- */
-
-minerva.models.GeoJSONStyle = Backbone.Model.extend({
-    defaults: {
-        // how is the style generated
-        scale: 'constant',   // 'constant' | 'continuous' | 'categorical'
-
-        // what type is the range
-        type: 'number',     // 'number' | 'color'
-
-        // which style attribute does it represent
-        name: null,         // 'fillColor' | 'fillOpacity' | etc...
-
-        // which property key is the scale applied to
-        key: null,
-
-        // for continuous scales
-        domain: [],         // numeric extent of the domain
-        range: [],          // numeric or color extent of the range
-
-        // for categorical scales
-        values: {},         // value -> (number | color) mapping
-
-        // for constant scales and default value
-        value: null
-    },
-
-    /**
-     * Return a d3 scale object that performs the style mapping indicated
-     * by this model's attributes.
-     */
-    scale: function () {
-        var scale, value, pairs;
-
-        switch (this.get('type')) {
-            case 'constant':
-                value = this.get('value');
-                scale = function () { return value; };
-                break;
-
-            case 'continuous':
-                scale = d3.scale.linear()
-                    .domain(this.get('domain'))
-                    .range(this.get('range'));
-                break;
-
-            case 'categorical':
-                pairs = _.pairs(this.get('values'));
-                scale = d3.scale.ordinal()
-                    .domain(pairs.map(_.first))
-                    .range(pairs.map(_.last));
-                break;
-
-            default:
-                throw new Error('Invalid style type');
-        }
-        return scale;
-    }
-});
-
 minerva.views.GeoJSONStyleWidget = minerva.View.extend({
     initialize: function () {
-        this._ramps = {};
 
-        _.each(colorbrewer, _.bind(function (ramp, name) {
-            var n = "<ul class='m-color-ramp'>";
-            _.each(ramp[6], function (color, i) {
-                n += "<li style='background-color: " + color + "'/>";
-            });
-            n += '</ul>';
-            this._ramps[name] = {
-                value: ramp[6],
-                display: n
-            };
-        }, this));
-        console.log(this._ramps);
-    },
-    render: function () {
-        var styles = [{
-            name: 'radius',
-            type: 'number',
-            value: 5,
-            scale: 'constant'
-        }, {
-            name: 'strokeWidth',
-            type: 'number',
-            value: 0,
-            scale: 'continuous',
-            key: 'count'
-        }, {
-            name: 'strokeColor',
-            type: 'color',
-            scale: 'continuous',
-            key: 'count'
-        }, {
-            name: 'fillColor',
-            type: 'color',
-            value: '#ff0000',
-            scale: 'constant'
-        }];
         var props = {
             county: {
                 values: {
@@ -134,12 +13,76 @@ minerva.views.GeoJSONStyleWidget = minerva.View.extend({
                 max: 75
             }
         };
-        var modal = this.$el.html(minerva.templates.geoJSONStyleWidget({
-            styles: styles,
-            properties: props,
-            ramps: this._ramps
-        })).girderModal(this).trigger(
-            $.Event('ready.girder.modal', {relatedTarget: modal})
-        ).find('select').selectpicker({width: '100%'});
+
+        this.collection = new minerva.collections.GeoJSONStyle([{
+            name: 'radius',
+            type: 'number',
+            value: 5,
+            scale: 'constant',
+            summary: props
+        }, {
+            name: 'strokeWidth',
+            type: 'number',
+            value: 0,
+            scale: 'continuous',
+            key: 'count',
+            summary: props
+        }, {
+            name: 'strokeColor',
+            type: 'color',
+            scale: 'continuous',
+            key: 'count',
+            ramp: 'Accent',
+            summary: props
+        }, {
+            name: 'fillColor',
+            type: 'color',
+            value: '#ff0000',
+            scale: 'constant',
+            summary: props
+        }]);
+
+        this.listenTo(this.collection, 'add', this.addOne);
+        this.listenTo(this.collection, 'reset', this.render);
+        this.listenTo(this.collection, 'remove', this.removeElement);
+    },
+    render: function (evt) {
+        if (evt && evt.norender) {
+            return;
+        }
+
+        var modal = this.$el.html(minerva.templates.geoJSONStyleWidget())
+            .girderModal(this)
+            .trigger(
+                $.Event('ready.girder.modal', {relatedTarget: modal})
+            );
+        this.addAll();
+        return this;
+    },
+
+    /**
+     * Add a style element to the widget.
+     */
+    addOne: function (model) {
+        var view = new minerva.views.GeoJSONStyleElement({
+            model: model,
+            parentView: this
+        });
+        this.$('.m-geojson-style').append(view.render().el);
+    },
+
+    /**
+     * Reset the view and add all style elements from the collection to the widget.
+     */
+    addAll: function () {
+        this.$('.m-geojson-style').children().remove();
+        this.collection.each(this.addOne, this);
+    },
+
+    /**
+     * Remove the style element from the widget.
+     */
+    removeElement: function (model) {
+        model.destroy();
     }
 });
