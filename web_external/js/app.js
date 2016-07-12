41a46c1be6ae12611e720b687e70d7d0a9dfd815
mgrauer <michael.grauer@kitware.com>
2016-07-12 23:32:45 +0000

    Remove minerva banner header

--- web_external/js/app.js
+++ web_external/js/app.js
@@ -3,11 +3,6 @@ minerva.App = girder.App.extend({
     render: function () {
         this.$el.html(minerva.templates.layout());
 
-        new minerva.views.LayoutHeaderView({
-            el: this.$('#m-app-header-container'),
-            parentView: this
-        }).render();
-
         return this;
     }
 
