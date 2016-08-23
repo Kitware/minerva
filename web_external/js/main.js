a6d96cd2c51f82297106420aaafdcd99d0ed8f23
mgrauer <michael.grauer@kitware.com>
2016-08-23 19:51:26 +0000

    Trigger app ready event from main

--- web_external/js/main.js
+++ web_external/js/main.js
@@ -4,5 +4,8 @@ $(function () {
         el: 'body',
         parentView: null
     });
+    minerva.mainApp.start().then(function () {
+        minerva.events.trigger('g:appload.ready');
+    });
     minerva.events.trigger('g:appload.after');
 });
