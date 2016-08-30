77a65ebe53242ec88fe4a16f54fe6da6f957177c
doruk.ozturk <doruk.ozturk@kitware.com>
2016-08-30 11:12:48 -0400

    Refactored duplicated code within the tests

--- plugin_tests/wms_test.py
+++ plugin_tests/wms_test.py
@@ -75,53 +75,9 @@ class WmsTestCase(base.TestCase):
             'minervauser', 'password', 'minerva', 'user',
             'minervauser@example.com')
 
-    def testCreateWmsSourceAndDataset(self):
-        """
-        Test the minerva WMS source and dataset API endpoints.
-        """
+    @classmethod
+    def setUpClass(cls):
 
-        # Create the source.
-        path = '/minerva_datasets_wms'
-        name = 'testWMS'
-        username = ''
-        password = ''
-        baseURL = 'http://fake.geoserver.fak/geoserver/ows'
-        params = {
-            'name': name,
-            'username': username,
-            'password': password,
-            'baseURL': baseURL
-        }
-
-        
-        with HTTMock(wms_mock):
-            response = self.request(path=path, method='POST', params=params, user=self._user)
-        self.assertStatusOk(response)
-        wmsSource = response.json
-
-        # Source generates multiple datasets now
-        source_type = [d['meta']['minerva']['source']['meta']['minerva']['source_type']
-                      for d in wmsSource]
-
-        base_url = [d['meta']['minerva']['source']['meta']['minerva']['wms_params']['base_url']
-                    for d in wmsSource]
-
-        source_name = [d['meta']['minerva']['source']['layer_source'] for d in wmsSource]
-        
-        self.assertTrue(len(set(source_type)) == 1 and source_type[0] == 'wms',
-                        'incorrect wms dataset typeName')
-        self.assertTrue(len(set(base_url)) == 1 and base_url[0] == baseURL,
-                        'incorrect wms dataset baseURL')
-        self.assertTrue(len(set(source_name)) == 1 and source_name[0] == name,
-                        'incorrect wms source name')
-
-
-    def testCreateWmsSourceWithAuthentication(self):
-        """
-        Enter a username and password for a WMS source and ensure that they
-        are correctly encrypted/decrypted.
-        """
-        
         # Create the source.
         path = '/minerva_datasets_wms'
         name = 'testWMS'
@@ -135,36 +91,60 @@ class WmsTestCase(base.TestCase):
             'baseURL': baseURL
         }
 
+        cls._path = path
+        cls._params = params
+        
+
+    def testCreateWmsSourceAndDataset(self):
+        """
+        Test the minerva WMS source and dataset API endpoints.
+        """
+
         with HTTMock(wms_mock):
-            response = self.request(path=path, method='POST', params=params, user=self._user)
+            response = self.request(path=self._path, method='POST', params=self._params, user=self._user)
+        self.assertStatusOk(response)
+        wmsSource = response.json
+
+        # Source generates multiple datasets now
+        source_type = [d['meta']['minerva']['source']['meta']['minerva']['source_type']
+                      for d in wmsSource]
+
+        base_url = [d['meta']['minerva']['source']['meta']['minerva']['wms_params']['base_url']
+                    for d in wmsSource]
+
+        source_name = [d['meta']['minerva']['source']['layer_source'] for d in wmsSource]
+        
+        self.assertEquals(set(source_type), {'wms'},
+                        'incorrect wms dataset typeName')
+        self.assertEquals(set(base_url), {self._params['baseURL']},
+                        'incorrect wms dataset baseURL')
+        self.assertEquals(set(source_name), {self._params['name']},
+                        'incorrect wms source name')
+
+
+    def testCreateWmsSourceWithAuthentication(self):
+        """
+        Enter a username and password for a WMS source and ensure that they
+        are correctly encrypted/decrypted.
+        """
+
+        with HTTMock(wms_mock):
+            response = self.request(path=self._path, method='POST', params=self._params, user=self._user)
         self.assertStatusOk(response)
         wmsSource = response.json
 
         from girder.plugins.minerva.utility.minerva_utility import decryptCredentials
-        
+
         credentials = [decryptCredentials(bytes(d['meta']['minerva']['credentials']))
                        for d in wmsSource]
 
-        self.assertTrue(len(set(credentials)) == 1 and
-                        credentials[0] == "{}:{}".format(username, password))
+        self.assertEquals(set(credentials), {"{}:{}".format(self._params['username'],
+                                                            self._params['password'])})
 
     def testCheckDatasetsExistInDatabase(self):
 
-        # Create the source.
-        path = '/minerva_datasets_wms'
-        name = 'testWMS'
-        username = ''
-        password = ''
-        baseURL = 'http://fake.geoserver.fak/geoserver/ows'
-        params = {
-            'name': name,
-            'username': username,
-            'password': password,
-            'baseURL': baseURL
-        }
-
         with HTTMock(wms_mock):
-            response = self.request(path=path, method='POST', params=params, user=self._user)
+            response = self.request(path=self._path, method='POST', params=self._params, user=self._user)
 
         self.assertStatusOk(response)
         from girder.plugins.minerva.utility.minerva_utility import findDatasetFolder
