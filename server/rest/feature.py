564e72135a07705b538a63143045b2cc7e8128be
doruk.ozturk <doruk.ozturk@kitware.com>
2016-10-28 13:57:59 -0400

    Added BSVE specific getfeatureinfo

--- server/rest/feature.py
+++ server/rest/feature.py
@@ -1,9 +1,13 @@
 from collections import defaultdict
+from urllib import quote
 
 from girder.api import access
 from girder.api.describe import Description
 from girder.api.rest import Resource
+from girder.plugins.minerva.utility.cookie import getExtraHeaders
+
 import requests
+
 
 class FeatureInfo(Resource):
 
@@ -50,6 +54,22 @@ class FeatureInfo(Resource):
 
         return req.content
 
+    @staticmethod
+    def callBsveFeatureInfo(baseUrl, params, typeNames):
+        """Call bsve api for getting information about
+        a lat long locaion"""
+
+        headers = getExtraHeaders()
+        headers.update({'Content-Type': 'application/xml'})
+
+        typeNames = ",".join(typeNames)
+
+        parameters = quote("$filter=names eq {} and query_layers eq {} and request eq getfeatureinfo and exceptions eq application/vnd.ogc.se_xml and feature_count eq 50 and projection eq EPSG:3857 and format eq image/png and geo.bbox eq {} and width eq {} and height eq {} and x eq {} and y eq {} and format_options eq callbak:getLayerFeatures&$format=application/json".format(typeNames, typeNames, params['bbox'], params['width'], params['height'], params['x'], params['y'] ), safe='=&$ ').replace(' ', '+')
+
+        req = requests.get(baseUrl, params=parameters, headers=headers)
+
+        return req.content
+
     @access.user
     def getFeatureInfo(self, params):
 
@@ -75,7 +95,7 @@ class FeatureInfo(Resource):
         grandResponse = []
         for baseUrl, layers in layerUrlMap.items():
             if 'bsvecosystem' in baseUrl:
-                pass
+                response = self.callBsveFeatureInfo(baseUrl, params, layers)
             else:
                 response = self.callFeatureInfo(baseUrl, params, layers)
             grandResponse.append(response)
