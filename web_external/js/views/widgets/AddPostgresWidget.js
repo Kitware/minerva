bd7f23364f76600161ecf5dffa9ba5c4b1a6d797
doruk.ozturk <doruk.ozturk@kitware.com>
2016-12-27 14:34:24 -0500

    Add handler for the g:created event and get tables from db

--- web_external/js/views/widgets/AddPostgresWidget.js
+++ web_external/js/views/widgets/AddPostgresWidget.js
@@ -2,20 +2,6 @@
 * This widget displays a form for adding a Postgresql database source.
  */
 minerva.views.AddPostgresWidget = minerva.View.extend({
-
-    addPostgresAssetstore: function (e, error, params) {
-        var assetstore = new girder.views.NewAssetstoreWidget();
-        // construct the uri here
-        var uri = 'postgresql://' + params.username +
-                ':' + params.password + '@' + params.server +
-                ':' + params.port + '/' + params.dbname;
-        assetstore.createAssetstore(e,
-                                    error,
-                                    {type: girder.AssetstoreType.DATABASE,
-                                     name: params.dbname,
-                                     dbtype: params.dbtype,
-                                     dburi: uri});
-    },
     events: {
         'submit #m-add-postgres-db-form': function (e) {
             var params = {
@@ -28,6 +14,30 @@ minerva.views.AddPostgresWidget = minerva.View.extend({
             };
             this.addPostgresAssetstore(e, this.$('#m-add-postgres-db-error'), params);
         }
+    },
+    addPostgresAssetstore: function (e, error, params) {
+        var assetstore = new girder.views.NewAssetstoreWidget();
+        // construct the uri here
+        var uri = 'postgresql://' + params.username +
+                ':' + params.password + '@' + params.server +
+                ':' + params.port + '/' + params.dbname;
+        assetstore.createAssetstore(e,
+                                    error,
+                                    {type: girder.AssetstoreType.DATABASE,
+                                     name: params.dbname,
+                                     dbtype: params.dbtype,
+                                     dburi: uri});
+        assetstore.on('g:created', function (astore) {
+            var _id = astore.id;
+            girder.restRequest({
+                path: '/database_assetstore' + '/' + _id + '/' + 'tables',
+                type: 'GET'
+            }).done(function (data) {
+                console.log(data);
+            });
+        });
+
+        this.$el.modal('hide');
     },
     render: function () {
         var modal = this.$el.html(minerva.templates.addPostgresWidget({})).girderModal(this);
