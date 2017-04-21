a6fe376a45b63925f229cf7b440511a969691922
doruk.ozturk <doruk.ozturk@kitware.com>
2017-04-21 13:14:31 -0400

    Add the endpoint to list and return geojson from postgres

--- server/rest/postgres_geojson.py
+++ server/rest/postgres_geojson.py
@@ -6,6 +6,7 @@ from girder.api.rest import Resource
 
 import psycopg2
 
+
 # TODO: This will be changed with girder_db_items
 def connect_to_gryphon(host="localhost",
                        port="5432",
@@ -13,58 +14,87 @@ def connect_to_gryphon(host="localhost",
                        password="password",
                        dbname="gryphon"):
 
-    try:
-        conn = psycopg2.connect("dbname={} user={} host={} password={} port={}".format(dbname,
-                                                                                       user,
-                                                                                       host,
-                                                                                       password,
-                                                                                       port))
-        return conn
-    except:
-        print "I am unable to connect to {}".format(dbname)
+    conn = psycopg2.connect("dbname={} user={} host={} password={} port={}".format(dbname,
+                                                                                   user,
+                                                                                   host,
+                                                                                   password,
+                                                                                   port))
+    return conn
+
+
+class PostgresGeojson(Resource):
+
+    def __init__(self):
+        self.resourceName = 'minerva_postgres_geojson'
+        self.route('GET', (), self.postgresGeojson)
+        self.route('GET', ('geojson', ), self.getGeojson)
+
+    @access.user
+    def postgresGeojson(self, params):
+        conn = connect_to_gryphon()
+        view = View(conn, params)
+        return view.filter()
+
+    @access.user
+    def getGeojson(self, params):
+        conn = connect_to_gryphon()
+        view = View(conn, params)
+        return view.getGeojson()
+
+    postgresGeojson.description = (
+        Description('Get geojson from postgres database')
+        .param('NAME', 'state name or all states', required=False,
+               dataType='list')
+        .param('PRODUCTION_CATEGORY', 'production category', required=False,
+               dataType='list')
+        .param('CATEGORY', 'category', required=False,
+               dataType='list')
+        .param('SUB_CATEGORY', 'sub category', required=False,
+               dataType='list')
+        .param('DATA_DERIVATION', 'data derivation', required=False,
+               dataType='list')
+    )
 
 
 class View(object):
-    def __init__(self, conn):
+    def __init__(self, conn, filters):
         self._conn = conn
+        self._filters = filters
 
-    def generateQuery(self, filters):
+    def generateQuery(self):
+        filters = self._filters
         q = []
         for k in filters.keys():
             _ = []
             for v in ast.literal_eval(filters[k]):
                 _.append(""" "{}" = '{}' """.format(k, v))
             q.append("(" + "or".join(_) + ")")
-        return "and".join(q)
-
+        return "and".join(q)q
 
     def getDistinctValues(self, table, filters={}):
-        conn = self._conn
-        cur = conn.cursor()
+        cur = self._conn.cursor()
         base_query = 'SELECT DISTINCT "{}" from gryphonstates'.format(table)
         if not filters:
-            query = base_query + ";"
+            query = base_query
         else:
-            query = base_query + " where" + self.generateQuery(filters) + ";"
-        cur.execute(query)
+            query = base_query + " where" + self.generateQuery()
+        cur.execute(query + ";")
         field = sorted([i[0] for i in cur.fetchall()])
-        if not filters:
-            field.insert(0, "All")
         return field
 
-    def filter(self, filters):
-
+    def filter(self):
+        filters = self._filters
         resp = {}
         resp['NAME'] = self.getDistinctValues('NAME', filters)
         resp['PRODUCTION_CATEGORY'] = self.getDistinctValues('PRODUCTION_CATEGORY', filters)
         resp['CATEGORY'] = self.getDistinctValues('CATEGORY', filters)
         resp['SUB_CATEGORY'] = self.getDistinctValues('SUB_CATEGORY', filters)
         resp['DATA_DERIVATION'] = self.getDistinctValues('DATA_DERIVATION', filters)
-
         return resp
 
     def getGeojson(self):
-        base = """SELECT "geom", "NAME", "PRODUCTION_CATEGORY", "VALUE" from gryphonstates"""
+        base = """SELECT "geom", "NAME", "PRODUCTION_CATEGORY", "VALUE", "CATEGORY",
+        "SUB_CATEGORY", "DATA_DERIVATION" from gryphonstates"""
         filters = self.generateQuery()
         query = base + " where " + filters
         cur = self._conn.cursor()
