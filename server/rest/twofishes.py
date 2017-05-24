ce82161dfcc1aa95febe601e331b8ba7044565ff
Doruk Ozturk <doruk.ozturk@kitware.com>
2017-05-24 13:03:15 -0400

    Add an endpoint which returns autocompleted results

--- server/rest/twofishes.py
+++ server/rest/twofishes.py
@@ -4,10 +4,12 @@ from girder.api import access
 from girder.api.describe import Description
 from girder.api.rest import Resource
 
+
 class TwoFishes(Resource):
     def __init__(self):
         self.resourceName = 'minerva_geocoder'
         self.route('GET', (), self.geocode)
+        self.route('GET', ('autocomplete',), self.autocomplete)
 
     @access.public
     def geocode(self, params):
@@ -20,4 +22,20 @@ class TwoFishes(Resource):
         Description('Get geojson for a given location name')
         .param('twofishes', 'Twofishes url')
         .param('location', 'Location name to get a geojson')
+    )
+
+    @access.public
+    def autocomplete(self, params):
+        r = requests.get(params['twofishes'],
+                         params={'autocomplete': True,
+                                 'query': params['location'],
+                                 'maxInterpretations': 10,
+                                 'autocompleteBias': None})
+
+        return [i['feature']['matchedName'] for i in r.json()['interpretations']]
+
+    autocomplete.description = (
+        Description('Autocomplete result for a given location name')
+        .param('twofishes', 'Twofishes url')
+        .param('location', 'Location name to autocomplete')
     )
