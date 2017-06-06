556f002fd4e1344ff1466cff50d929eccaf70a39
Doruk Ozturk <doruk.ozturk@kitware.com>
2017-06-06 15:30:37 -0400

    Improve type checking and add docstring

--- server/rest/twofishes.py
+++ server/rest/twofishes.py
@@ -1,3 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+###############################################################################
+#  Copyright Kitware Inc.
+#
+#  Licensed under the Apache License, Version 2.0 ( the "License" );
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+###############################################################################
+
+import ast
 import json
 import StringIO
 
@@ -14,6 +34,7 @@ from girder.plugins.minerva.utility.minerva_utility import findDatasetFolder
 
 
 class TwoFishes(Resource):
+    """Resource that handles geocoding related operations"""
     def __init__(self):
         self.resourceName = 'minerva_geocoder'
         self.route('GET', ('autocomplete',), self.autocomplete)
@@ -38,6 +59,12 @@ class TwoFishes(Resource):
     @staticmethod
     def createGeojson(twofishes, locations):
         """Create geojson for given locations and twofishes url"""
+
+        try:
+            locations = ast.literal_eval(locations)
+        except ValueError:
+            pass
+
         geoms = []
 
         for i in locations:
@@ -56,7 +83,7 @@ class TwoFishes(Resource):
         output = StringIO.StringIO(json.dumps(geojsonString))
         outputSize = output.len
         user = self.getCurrentUser()
-        datasetFolder = findDatasetFolder(user, user)
+        datasetFolder = findDatasetFolder(user, user, create=True)
         itemModel = ModelImporter.model('item')
         uploadModel = ModelImporter.model('upload')
         item = itemModel.createItem(name, user, datasetFolder)
