09a2b38587b1885a30afa93a86cfec7e72dec29d
Matthew Ma <matthew.ma@kitware.com>
2017-11-08 18:24:46 -0500

    Make setup.py install pip requirements.txt

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup
+from pip.req import parse_requirements
+
+install_reqs = parse_requirements('requirements.txt', session=False)
+reqs = [str(ir.req) for ir in install_reqs]
 
 setup(name='minerva.geo',
       version='0.0.0.dev1',
       description='Minerva: client/server/services for analysis and visualization',
       url='https://github.com/kitware/minerva',
+      install_requires=reqs,
       author='Kitware Inc',
       author_email='minerva-developers@kitware.com',
       license='Apache v2',
