e3b798b77a9a0d5e72423487a9e092beed4a3caf
Matthew Ma <matthew.ma@kitware.com>
2017-11-10 13:23:41 -0500

    Uses a table layout to show some meta of dataset instead of showing the raw json

--- web_external/views/widgets/DatasetInfoWidget.js
+++ web_external/views/widgets/DatasetInfoWidget.js
@@ -1,29 +1,41 @@
-import _ from 'underscore';
-
 import View from '../view';
 import template from '../../templates/widgets/datasetInfoWidget.pug';
+import '../../stylesheets/widgets/datasetInfoWidget.styl';
 /**
 * This widget is used to diplay minerva metadata for a dataset.
 */
 const DatasetInfoWidget = View.extend({
     initialize: function (settings) {
         this.dataset = settings.dataset;
+        this.normalizeMetaInfo = this.normalizeMetaInfo.bind(this);
     },
 
     render: function () {
-        var modal = this.$el.html(template({}))
-            .girderModal(this)
-            .on('ready.girder.modal',
-            _.bind(function () {
-                this.$('#datasetInfo')
-                    .text(JSON.stringify(
-                        this.dataset.get('meta').minerva,
-                        null, 4));
-            }, this));
+        var modal = this.$el.html(template(this))
+            .girderModal(this);
 
         modal.trigger($.Event('ready.girder.modal', { relatedTarget: modal }));
 
         return this;
+    },
+    normalizeMetaInfo() {
+        var meta = this.dataset.get('meta').minerva;
+        var output = {
+            Name: meta.geojson_file.name,
+            Source: 'file'
+        };
+        switch (meta.dataset_type) {
+            case 'geojson':
+                output.Type = 'GeoJSON';
+                if (meta.postgresGeojson) {
+                    output.Source = 'PostgreSQL';
+                }
+                break;
+            case 'geotiff':
+                output.Type = 'GeoTiff';
+                break;
+        }
+        return output;
     }
 });
 export default DatasetInfoWidget;
