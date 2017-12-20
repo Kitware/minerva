d250269d2bc3e442fb84f8df8ef017be578e6fed
Matthew Ma <matthew.ma@kitware.com>
2017-12-20 12:26:47 -0500

    Remove unused plugin config

--- web_client/main.js
+++ web_client/main.js
@@ -1,13 +1,1 @@
-import events from 'girder/events';
-import router from 'girder/router';
-import { exposePluginConfig } from 'girder/utilities/PluginUtils';
-
-import ConfigView from './views/ConfigView';
-
 import './views/fileList';
-
-exposePluginConfig('minerva', 'plugins/minerva/config');
-
-router.route('plugins/minerva/config', 'minervaConfig', function () {
-    events.trigger('g:navigateTo', ConfigView);
-});
