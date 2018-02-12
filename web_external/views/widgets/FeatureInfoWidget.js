7cf45fd9a6ce1435bcb6003e6e9c4285ba5700ad
Doruk Ozturk <doruk.ozturk@kitware.com>
2018-02-11 19:46:54 -0500

    Query pixel values with large_image /pixel endpoint

--- web_external/views/widgets/FeatureInfoWidget.js
+++ web_external/views/widgets/FeatureInfoWidget.js
@@ -156,14 +156,16 @@ const FeatureInfoWidget = View.extend({
             return $.when([]);
         } else {
             var infoDeferreds = geotiffLayers.map((dataset) => {
-                let fileId = dataset.get('meta').minerva.original_files[0]._id;
-                let coord = event.geo;
+                let itemId = dataset.get('_id');
+                let coord = event.mapgcs;
                 return restRequest({
-                    url: `ktile/${fileId}/query`,
+                    url: `item/${itemId}/tiles/pixel`,
                     type: 'GET',
                     data: {
-                        'lat': coord.y,
-                        'lon': coord.x
+                        'top': coord.y,
+                        'left': coord.x,
+			'projection': 'EPSG:3857',
+			'units': 'projection'
                     }
                 }).then((result) => {
                     if (_.isEmpty(result)) {
@@ -171,7 +173,7 @@ const FeatureInfoWidget = View.extend({
                     }
                     return {
                         id: dataset.get('name'),
-                        properties: result
+                        properties: result.bands
                     };
                 });
             });
