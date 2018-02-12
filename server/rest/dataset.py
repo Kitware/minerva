720fcac6aa6aa9a3dcd984c8c7808040da3649c7
Doruk Ozturk <doruk.ozturk@kitware.com>
2018-02-12 09:01:13 -0500

    Refactor update metadata logic into it's own function

--- server/rest/dataset.py
+++ server/rest/dataset.py
@@ -224,6 +224,79 @@ class Dataset(Resource):
         updateMinervaMetadata(dataset, minerva_metadata)
         return dataset
 
+    def _updateMinervaMetadata(self, item):
+        minerva_metadata = {
+            'source_type': 'item'
+        }
+        for file in self.model('item').childFiles(item=item, limit=0):
+            # Check the first few k of a file to see if this might be a
+            # geojson timeseries.  Crudely, we expect this to be a json array
+            # which contains objects, each of which has at least a geojson
+            # element.  This test will fail if there are other elements in the
+            # first object that push the geojson element beyond the tested
+            # header length.  It could give a false positive, too.  The correct
+            # way would be to download and parse the whole file, but that would
+            # be more expensive in memory and time.
+            headerLen = 2048
+            fileHeader = ''
+            for headerData in self.model('file').download(file, headers=False, endByte=headerLen)():
+                fileHeader = (fileHeader + headerData)[:headerLen]
+                if len(fileHeader) >= headerLen:
+                    break
+            if (fileHeader.lstrip()[:1] == '[' and
+                    fileHeader.lstrip()[1:].lstrip()[:1] == '{' and
+                    '"geojson"' in fileHeader):
+                minerva_metadata['original_type'] = 'geojson-timeseries'
+                minerva_metadata['dataset_type'] = 'geojson-timeseries'
+                minerva_metadata['original_files'] = [{
+                    'name': file['name'], '_id': file['_id']}]
+                minerva_metadata['geojson_file'] = {
+                    'name': file['name'], '_id': file['_id']}
+                minerva_metadata['source'] = {
+                    'layer_source': 'GeoJSON'}
+                break
+            # TODO This switching based on which file is found first is
+            # fairly brittle and should only be called after first upload.
+            if 'geojson' in file['exts']:
+                # we found a geojson, assume this is geojson original
+                minerva_metadata['original_type'] = 'geojson'
+                minerva_metadata['dataset_type'] = 'geojson'
+                minerva_metadata['original_files'] = [{
+                    'name': file['name'], '_id': file['_id']}]
+                minerva_metadata['geojson_file'] = {
+                    'name': file['name'], '_id': file['_id']}
+                minerva_metadata['source'] = {
+                    'layer_source': 'GeoJSON'}
+                break
+            elif 'json' in file['exts']:
+                minerva_metadata['original_type'] = 'json'
+                minerva_metadata['dataset_type'] = 'json'
+                minerva_metadata['original_files'] = [{
+                    'name': file['name'], '_id': file['_id']}]
+                break
+            elif 'csv' in file['exts']:
+                minerva_metadata['original_type'] = 'csv'
+                minerva_metadata['dataset_type'] = 'csv'
+                minerva_metadata['original_files'] = [{
+                    'name': file['name'], '_id': file['_id']}]
+                break
+            elif ({'tif', 'tiff'}.intersection(file['exts']) and
+                  file['mimeType'] == 'image/tiff'):
+                if not self.client:
+                    self._initClient()
+                info = self.client.get(path='/item/%s/tiles' % str(item['_id']))
+                if 'srs' in info['sourceBounds'] and info['sourceBounds']['srs']:
+                    minerva_metadata['original_type'] = 'tiff'
+                    minerva_metadata['dataset_type'] = 'geotiff'
+                    minerva_metadata['original_files'] = [{
+                        'name': file['name'], '_id': file['_id']}]
+                    minerva_metadata['source'] = {
+                        'layer_source': 'Tiff'}
+                break
+        updateMinervaMetadata(item, minerva_metadata)
+
+        return minerva_metadata
+
     # REST Endpoints
 
     @access.public
@@ -367,75 +440,7 @@ class Dataset(Resource):
         if 'meta' in item and 'minerva' in item['meta']:
             return item
 
-        minerva_metadata = {
-            'source_type': 'item'
-        }
-        for file in self.model('item').childFiles(item=item, limit=0):
-            # Check the first few k of a file to see if this might be a
-            # geojson timeseries.  Crudely, we expect this to be a json array
-            # which contains objects, each of which has at least a geojson
-            # element.  This test will fail if there are other elements in the
-            # first object that push the geojson element beyond the tested
-            # header length.  It could give a false positive, too.  The correct
-            # way would be to download and parse the whole file, but that would
-            # be more expensive in memory and time.
-            headerLen = 2048
-            fileHeader = ''
-            for headerData in self.model('file').download(file, headers=False, endByte=headerLen)():
-                fileHeader = (fileHeader + headerData)[:headerLen]
-                if len(fileHeader) >= headerLen:
-                    break
-            if (fileHeader.lstrip()[:1] == '[' and
-                    fileHeader.lstrip()[1:].lstrip()[:1] == '{' and
-                    '"geojson"' in fileHeader):
-                minerva_metadata['original_type'] = 'geojson-timeseries'
-                minerva_metadata['dataset_type'] = 'geojson-timeseries'
-                minerva_metadata['original_files'] = [{
-                    'name': file['name'], '_id': file['_id']}]
-                minerva_metadata['geojson_file'] = {
-                    'name': file['name'], '_id': file['_id']}
-                minerva_metadata['source'] = {
-                    'layer_source': 'GeoJSON'}
-                break
-            # TODO This switching based on which file is found first is
-            # fairly brittle and should only be called after first upload.
-            if 'geojson' in file['exts']:
-                # we found a geojson, assume this is geojson original
-                minerva_metadata['original_type'] = 'geojson'
-                minerva_metadata['dataset_type'] = 'geojson'
-                minerva_metadata['original_files'] = [{
-                    'name': file['name'], '_id': file['_id']}]
-                minerva_metadata['geojson_file'] = {
-                    'name': file['name'], '_id': file['_id']}
-                minerva_metadata['source'] = {
-                    'layer_source': 'GeoJSON'}
-                break
-            elif 'json' in file['exts']:
-                minerva_metadata['original_type'] = 'json'
-                minerva_metadata['dataset_type'] = 'json'
-                minerva_metadata['original_files'] = [{
-                    'name': file['name'], '_id': file['_id']}]
-                break
-            elif 'csv' in file['exts']:
-                minerva_metadata['original_type'] = 'csv'
-                minerva_metadata['dataset_type'] = 'csv'
-                minerva_metadata['original_files'] = [{
-                    'name': file['name'], '_id': file['_id']}]
-                break
-            elif ({'tif', 'tiff'}.intersection(file['exts']) and
-                  file['mimeType'] == 'image/tiff'):
-                if not self.client:
-                    self._initClient()
-                info = self.client.get(path='/item/%s/tiles' % str(item['_id']))
-                if 'srs' in info['sourceBounds'] and info['sourceBounds']['srs']:
-                    minerva_metadata['original_type'] = 'tiff'
-                    minerva_metadata['dataset_type'] = 'geotiff'
-                    minerva_metadata['original_files'] = [{
-                        'name': file['name'], '_id': file['_id']}]
-                    minerva_metadata['source'] = {
-                        'layer_source': 'Tiff'}
-                break
-        updateMinervaMetadata(item, minerva_metadata)
+        minerva_metadata = self._updateMinervaMetadata(item)
         bounds = self._getBound(item)
         if bounds:
             minerva_metadata['bounds'] = bounds
