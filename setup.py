dba6b2441d5bc220120d1a3b013bda79f7b4b718
David Manthey <david.manthey@kitware.com>
2018-03-21 16:07:07 -0400

    Specify Gaia explicitly from github.
    Update how setup.py parses requirements.txt.

--- setup.py
+++ setup.py
@@ -1,14 +1,16 @@
 from setuptools import setup, find_packages
 from pip.req import parse_requirements
 
-install_reqs = parse_requirements('requirements.txt', session=False)
-reqs = [str(ir.req) for ir in install_reqs]
+install_reqs = list(parse_requirements('requirements.txt', session=False))
+reqs = [str(ir.req) if not ir.original_link else str(ir.req).split('-')[0] for ir in install_reqs]
+dep_links = [str(ir.original_link) for ir in install_reqs if ir.original_link]
 
 setup(name='minerva.geo',
       version='0.0.0.dev1',
       description='Minerva: client/server/services for analysis and visualization',
       url='https://github.com/kitware/minerva',
       install_requires=reqs,
+      dependency_links=dep_links,
       author='Kitware Inc',
       author_email='minerva-developers@kitware.com',
       license='Apache v2',
